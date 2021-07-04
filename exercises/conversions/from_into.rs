b384190ac12337db28bda63ffc7222f7a1165471
rgb-24bit <rgb-24bit@foxmail.com>
2021-07-04 19:23:58 +0800

    feat: complete exercises
    🎉 All exercises completed! 🎉
    
    +----------------------------------------------------+
    |          You made it to the Fe-nish line!          |
    +--------------------------  ------------------------+
                              \/
         ▒▒          ▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒▒          ▒▒
       ▒▒▒▒  ▒▒    ▒▒        ▒▒  ▒▒        ▒▒    ▒▒  ▒▒▒▒
       ▒▒▒▒  ▒▒  ▒▒            ▒▒            ▒▒  ▒▒  ▒▒▒▒
     ░░▒▒▒▒░░▒▒  ▒▒            ▒▒            ▒▒  ▒▒░░▒▒▒▒
       ▓▓▓▓▓▓▓▓  ▓▓      ▓▓██  ▓▓  ▓▓██      ▓▓  ▓▓▓▓▓▓▓▓
         ▒▒▒▒    ▒▒      ████  ▒▒  ████      ▒▒░░  ▒▒▒▒
           ▒▒  ▒▒▒▒▒▒        ▒▒▒▒▒▒        ▒▒▒▒▒▒  ▒▒
             ▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒▒▒▒▒▒▒▒▓▓▒▒▓▓▒▒▒▒▒▒▒▒
               ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
                 ▒▒▒▒▒▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒
               ▒▒  ▒▒▒▒▒▒▒▒▒▒██████▒▒▒▒▒▒▒▒▒▒  ▒▒
             ▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒
           ▒▒    ▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    ▒▒    ▒▒
           ▒▒  ▒▒    ▒▒                  ▒▒    ▒▒  ▒▒
               ▒▒  ▒▒                      ▒▒  ▒▒
    
    We hope you enjoyed learning about the various aspects of Rust!
    If you noticed any issues, please don't hesitate to report them to our repo.
    You can also contribute your own exercises to help the greater community!
    
    Before reporting an issue or contributing, please read our guidelines:
    https://github.com/rust-lang/rustlings/blob/main/CONTRIBUTING.md

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default();
+        }
+
+        let parts: Vec<&str> = s.split(",").collect();
+
+        if parts.len() != 2 || parts[0].len() == 0 || parts[1].len() == 0 {
+            return Person::default();
+        }
+
+        let age = match parts[1].parse::<usize>() {
+            Ok(age) => age,
+            _ => {return Person::default();}
+        };
+
+        Person{
+            name: parts[0].to_string(),
+            age: age,
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -11,8 +11,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -26,6 +24,25 @@ struct Person {
 impl FromStr for Person {
     type Err = Box<dyn error::Error>;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err("".into());
+        }
+
+        let parts: Vec<&str> = s.split(",").collect();
+
+        if parts.len() != 2 || parts[0].len() == 0 || parts[1].len() == 0 {
+            return Err("".into());
+        }
+
+        let age = match parts[1].parse::<usize>() {
+            Ok(age) => age,
+            _ => {return return Err("".into());}
+        };
+
+        Ok(Person{
+            name: parts[0].to_string(),
+            age: age,
+        })
     }
 }
 
